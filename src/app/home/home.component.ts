a7de46a325fbcc2f0aed97aee51c6488954137ed
Scott Ian <sim2487@gmail.com>
2019-07-12 10:39:05 -0700

    list populating correctly

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,13 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   
-  itemCount: number = 4; //interpolation
+  itemCount: number; //interpolation
   btnText: string = 'Add an item'; //one-way data binding / property binding. see html template for <p> element
   itemText: string = 'My first item'; //two-way data binding. see template <input> element. requires formsModule in app.module
+  items = []; //array to hold items added by user
 
   constructor() { }
 
   ngOnInit() {
+    this.itemCount = this.items.length; //set item count when page loads
   }
+  
+  addItem() {
+    //when add item button is pushed do this:
+    this.items.push(this.itemText); //add the new item to the items array
+    this.itemText = ''; //clear out the input field
+    this.itemCount = this.items.length; //update the item count
+  }
+  
 
 }
