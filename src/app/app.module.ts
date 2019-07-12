39c2b13f909adffeba19946db2f0754c1b9968c1
Scott Ian <sim2487@gmail.com>
2019-07-12 08:58:54 -0700

    data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
@@ -16,7 +16,8 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  
+  itemCount: number = 4; //interpolation
+  btnText: string = 'Add an item'; //one-way data binding / property binding. see html template for <p> element
+  itemText: string = 'My first item'; //two-way data binding. see template <input> element. requires formsModule in app.module
 
   constructor() { }
 
